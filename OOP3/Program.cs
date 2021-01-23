aeddb4dbde9098f203bc74745a215eac2e8b8a17
ALI TURGUT YILDIRIM <AYILDIRIM16@ku.edu.tr>
2021-01-23 23:51:47 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new DataBaseLoggerService());
+            basvuruManager.BasvuruYap(tasitKrediManager, databaseLoggerService);
+            basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager, konutKrediManager };
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
